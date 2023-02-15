f1752caa95fe76b98759ced245878bd60eb52d3b
julius-ek-hub <ekanejulius7@gmail.com>
2023-02-15 11:42:06 +0400

    removed tmp file

--- ofd.ps1
+++
@@ -1,6 +0,0 @@
-[System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | out-null 
-$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog 
-$OpenFileDialog.initialDirectory = "C:\Users\juliu\Downloads" 
-$OpenFileDialog.filter = "Excel workbooks (*.xlsx)|*.xlsx|CSV files (*.csv)|*.csv" 
-$OpenFileDialog.ShowDialog() 
-$OpenFileDialog.filename 
